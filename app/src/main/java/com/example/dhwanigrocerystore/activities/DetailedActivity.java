db7667bad6b21b6051b35499821eadc64016ae4e
u-dhwani <sdkkupadhyay002@gmail.com>
2022-02-24 20:36:19 +0530

    Uploading files

--- app/src/main/java/com/example/dhwanigrocerystore/activities/DetailedActivity.java
+++ app/src/main/java/com/example/dhwanigrocerystore/activities/DetailedActivity.java
@@ -2,10 +2,12 @@ package com.example.dhwanigrocerystore.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.example.dhwanigrocerystore.R;
 
@@ -14,16 +16,25 @@ public class DetailedActivity extends AppCompatActivity {
     TextView price,rating,description;
     Button addToCart;
     ImageView addItem,removeItem;
+    Toolbar toolbar;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed);
-        ImageView=findViewById(R.id.detailed_img);
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        detailedImg=findViewById(R.id.detailed_img);
         addItem=findViewById(R.id.add_item);
         removeItem=findViewById(R.id.remove_item);
         price=findViewById(R.id.detailed_price);
-        rating=findViewById(R.id.detailed_img);
-        ImageView=findViewById(R.id.detailed_img);
-        ImageView=findViewById(R.id.detailed_img);
+        rating=findViewById(R.id.detailed_rating_text);
+        description=findViewById(R.id.detailed_description);
+        addToCart=findViewById(R.id.add_to_cart);
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
     }
 }
